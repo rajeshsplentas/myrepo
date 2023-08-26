1f570a7228773a275c221472168cbaa8881d36b4
Rajesh Ranjan <rajeshsplentas@gmail.com>
2023-08-26 08:43:26 +0530

    First Commit by Rajesh Ranjan on Spring Boot applications

---
+++ src/main/java/io/javabrains/springbootstarter/topic/PropertySourceConfiguration.java
@@ -0,0 +1,16 @@
+
+/*
+ * package io.javabrains.springbootstarter.topic;
+ * 
+ * import org.springframework.beans.factory.annotation.Value; import
+ * org.springframework.context.annotation.Configuration; import
+ * org.springframework.context.annotation.PropertySource;
+ * 
+ * @Configuration
+ * 
+ * @PropertySource("file:c:/properties/application.properties") public class
+ * PropertySourceConfiguration {
+ * 
+ * @Value("${app.name}") public void getName(String name) {
+ * System.out.println("Load from external " + name); } }
+ */
---
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -0,0 +1,54 @@
+package io.javabrains.springbootstarter.topic;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+	
+	@Autowired
+	private TopicService topicService;
+
+	@RequestMapping("/topics")
+	public List<Topic> getAllTopics() {
+		return topicService.getAllTopics();
+	}
+	
+	@RequestMapping("/topics/{id}")
+	@Cacheable(key = "#id", value = "java")
+	public Optional<Topic> getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,value="/topics")
+	public void addTopic(@RequestBody Topic topic) {
+	
+		topicService.addTopic(topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT,value="/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+	
+		topicService.updateTopic(id,topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE,value="/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
+	}
+	
+	@RequestMapping("/secured")
+	  public String secured(){
+	    System.out.println("Inside secured()");
+	    return "Hello user !!! : " + new Date();
+	  }
+}
